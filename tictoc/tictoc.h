3b669e55c9b0bb75f623a8918fee1eedf3fe888e
Robin Martinjak <rob@rmartinjak.de>
2014-02-03 09:40:05 +0100

    add WTFPL to tictoc.h

--- tictoc/tictoc.h
+++ tictoc/tictoc.h
@@ -1,3 +1,11 @@
+/* tictoc.h - a MATLAB-like "stopwatch"
+ *
+ * Copyright (c) 2014 Robin Martinjak <rob@rmartinjak.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+
 #ifndef TICTOC_H
 #define TICTOC_H
 
