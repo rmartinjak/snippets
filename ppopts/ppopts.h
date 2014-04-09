38e4bc4ede9ca3f4a44c10f03850afb8d6466f08
Robin Martinjak <rob@rmartinjak.de>
2014-04-09 15:04:07 +0200

    ppopts: add a bit of documentation

--- ppopts/ppopts.h
+++ ppopts/ppopts.h
@@ -1,8 +1,56 @@
 #ifndef PPOPTS_H
 #define PPOPTS_H
 
+/** Pretty-print command line options.
+ *
+ * This module tries its best to print command-line options in a visually
+ * pleasing way:
+ *
+ * - Options, arguments and description texts are horizontally aligned.
+ * - Descriptions are automatically wrapped (at word boundaries) to not exceed
+ *   the given width (except single words that don't fit)
+ * - Print descriptions either on the same line as the options or on the
+ *   following one.
+ * - Option descriptions containing at least one newline ('\n') character are
+ *   always printed literally (on the line after the options), indented by a
+ *   few spaces before each line. This allows user-defined formatting mixed
+ *   with automatic formatting.
+ * - Optionally hide all long descriptions (e.g. if getopt_long() is not
+ *   available)
+ *
+ *
+ *
+ * Example code:
+ *
+ *  struct ppopts o;
+ *  ppopts_add(&o, 'o', "opt", "A", "Doe something with A.");
+ *  ppopts_add(&o, 'f,, "filename", "FILE",
+ *             "Read input from FILE. If not specified use standard input.");
+ *  ppopts_add(&o, 'd', "debug", "", "Enable debug mode.");
+ *  ppopts_add(&o, 'l', "literal, "",
+ *             "An option with\n   literal description text.");
+ *  ppopts_print(&o, stderr, 75, 0);
+ *
+ *
+ * Output:
+ *
+ *  -o A     --opt      A       Do something with A.
+ *
+ *  -f FILE  --filename FILE    Read input from FILE. If not specified use
+ *                              standard input.
+ *
+ *  -d       --debug            Enable debug mode.
+ *
+ *  -l       --literal
+ *      An option with
+ *          literal description text.
+ */
 
+
+/** Don't pring long options */
 #define PPOPTS_NO_LONGOPTS (1 << 0)
+
+/** Always print the descriptions on the next line */
 #define PPOPTS_DESC_ON_NEXT_LINE (1 << 1)
 
 
